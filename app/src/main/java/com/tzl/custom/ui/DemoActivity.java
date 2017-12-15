491f312a9b9728c7736fe82ad563ce1a319b2988
tzl <15036305782@163.com>
2017-12-15 23:22:07 +0800

    点赞动画

---
+++ app/src/main/java/com/tzl/custom/ui/DemoActivity.java
@@ -0,0 +1,15 @@
+package com.tzl.custom.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tzl.custom.R;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
