58a7a11b77649736a53d91e56c7bc9a4162c03de
tzl <15036305782@163.com>
2017-12-16 22:42:11 +0800

    点赞动画

--- app/src/main/java/com/tzl/custom/ui/DemoActivity.java
+++ app/src/main/java/com/tzl/custom/ui/DemoActivity.java
@@ -11,5 +11,6 @@ public class DemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
+
     }
 }
